7c030e48c33c68ecffc5e1b5935b64162675cd1c
xxoolm <30407581+xxoolm@users.noreply.github.com>
2021-04-02 22:12:56 +0800

    补充

--- foo_ui_columns/config_artwork.cpp
+++ foo_ui_columns/config_artwork.cpp
@@ -31,9 +31,9 @@ public:
     {
         m_initialising = true;
         const HWND wnd_combo = GetDlgItem(wnd, IDC_EDGESTYLE);
-        ComboBox_AddString(wnd_combo, L"None");
-        ComboBox_AddString(wnd_combo, L"Sunken");
-        ComboBox_AddString(wnd_combo, L"Grey");
+        ComboBox_AddString(wnd_combo, L"无");
+        ComboBox_AddString(wnd_combo, L"凹陷");
+        ComboBox_AddString(wnd_combo, L"灰白");
         ComboBox_SetCurSel(wnd_combo, artwork_panel::cfg_edge_style);
         m_initialising = false;
     }
