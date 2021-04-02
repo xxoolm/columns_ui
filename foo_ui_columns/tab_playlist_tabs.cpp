895efd317907c982f3559dca360efe9ce3274e32
xxoolm <30407581+xxoolm@users.noreply.github.com>
2021-04-02 22:46:23 +0800

    播放列表标签汉化

--- foo_ui_columns/tab_playlist_tabs.cpp
+++ foo_ui_columns/tab_playlist_tabs.cpp
@@ -46,7 +46,7 @@ public:
         return m_helper.create(wnd, IDD_PREFS_PLAYLIST_TABS,
             [this](auto&&... args) { return ConfigProc(std::forward<decltype(args)>(args)...); });
     }
-    const char* get_name() override { return "Playlist tabs"; }
+    const char* get_name() override { return (char*)u8"标签"; }
     bool get_help_url(pfc::string_base& p_out) override
     {
         p_out = "http://yuo.be/wiki/columns_ui:config:playlist_switcher:general";
